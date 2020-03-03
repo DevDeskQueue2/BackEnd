89678aafe8d5ebecca7b2681427cd37cad55bf48
Hudson Chamberlain <hudson.m.chamberlain@gmail.com>
2020-03-02 22:15:59 -0600

    get login and register working

--- data/db.js
+++ data/db.js
@@ -3,6 +3,5 @@ const knex = require('knex');
 const knexfile = require('../knexfile');
 
 const env = process.env.NODE_ENV || 'development';
-const configOptions = knexfile[env];
 
-module.exports = knex(configOptions);
+module.exports = knex(knexfile[env]);
